546bb8bbe01a67a9db9c47090be6db0abb1c0795
Ernesto Amaral <erneto.amaral13@gmail.com>
2025-08-06 00:49:46 -0700

    Set application theme to Light and apply database migrations on initialization

--- ChecadorComunicacion/App.axaml.cs
+++ ChecadorComunicacion/App.axaml.cs
@@ -1,14 +1,21 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using ChecadorComunicacion.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ChecadorComunicacion;
 
 public partial class App : Application
 {
+    
+    private readonly AppDbContext _db = new();
+    
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+        _db.Database.Migrate();
+        
     }
 
     public override void OnFrameworkInitializationCompleted()
