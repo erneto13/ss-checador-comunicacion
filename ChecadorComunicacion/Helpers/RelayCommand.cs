59b068fa8ec2d6a6c83d3f6cdfb5d2759094c680
Ernesto Amaral <erneto.amaral13@gmail.com>
2025-08-06 00:50:34 -0700

    Implement RelayCommand class with ICommand interface for command handling

--- ChecadorComunicacion/Helpers/RelayCommand.cs
+++ ChecadorComunicacion/Helpers/RelayCommand.cs
@@ -1,6 +1,22 @@
-﻿namespace ChecadorComunicacion.Helpers;
+﻿using System;
+using System.Windows.Input;
 
-public class RelayCommand
+namespace ChecadorComunicacion.Helpers;
+
+public class RelayCommand : ICommand
 {
-    
+    private readonly Action _execute;
+    private readonly Func<bool> _canExecute;
+
+    public RelayCommand(Action execute, Func<bool>? canExecute = null)
+    {
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object? parameter) => _canExecute?.Invoke() ?? true;
+
+    public void Execute(object? parameter) => _execute();
+
+    public event EventHandler? CanExecuteChanged;
 }
