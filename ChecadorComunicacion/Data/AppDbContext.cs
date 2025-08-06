26fe5bdd4152f21ff73c92c59d6ba9d694d2624b
Ernesto Amaral <erneto.amaral13@gmail.com>
2025-08-06 00:49:58 -0700

    Add DbSet for Personas and configure MySQL connection in AppDbContext

--- ChecadorComunicacion/Data/AppDbContext.cs
+++ ChecadorComunicacion/Data/AppDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace ChecadorComunicacion.Data;
+﻿using ChecadorComunicacion.Services;
+using Microsoft.EntityFrameworkCore;
 
-public class AppDbContext
+namespace ChecadorComunicacion.Data;
+
+public class AppDbContext : DbContext
 {
-    
+    public DbSet<Persona> Personas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "server=localhost;user=root;password=admin;database=checador_curn";
+        optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    }
 }
