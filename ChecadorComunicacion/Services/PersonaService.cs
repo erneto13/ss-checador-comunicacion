5730b4ef062e6d4df4d0ceca608d2b8abf1a5cdb
Ernesto Amaral <erneto.amaral13@gmail.com>
2025-08-06 00:50:20 -0700

    Implement PersonaService with CRUD operations for managing Personas

--- ChecadorComunicacion/Services/PersonaService.cs
+++ ChecadorComunicacion/Services/PersonaService.cs
@@ -1,6 +1,42 @@
-﻿namespace ChecadorComunicacion.Services;
+﻿using System.Collections.Generic;
+using System.Linq;
+using ChecadorComunicacion.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChecadorComunicacion.Services;
 
 public class PersonaService
 {
-    
+    private readonly AppDbContext _context = new AppDbContext();
+
+    public List<Persona> ObtenerPersonas()
+    {
+        return _context.Personas.AsNoTracking().ToList();
+    }
+
+    public void AgregarPersona(Persona persona)
+    {
+        _context.Personas.Add(persona);
+        _context.SaveChanges();
+    }
+
+    public void ActualizarPersona(Persona persona)
+    {
+        using var context = new AppDbContext();
+        context.Attach(persona);
+        context.Entry(persona).State = EntityState.Modified;
+        context.SaveChanges();
+    }
+
+    public void EliminarPersona(Persona persona)
+    {
+        var entidad = _context.Personas.FirstOrDefault(p => p.Id == persona.Id)
+                      ?? _context.Personas.Find(persona.Id);
+
+        if (entidad != null)
+        {
+            _context.Personas.Remove(entidad);
+            _context.SaveChanges();
+        }
+    }
 }
