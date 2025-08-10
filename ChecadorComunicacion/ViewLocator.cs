fe13e256fd5f8407e1cf55ead7a499d53395ec46
Ernesto Amaral <erneto.amaral13@gmail.com>
2025-08-09 18:42:50 -0700

    Add ViewLocator for dynamic view resolution based on ViewModel types

---
+++ ChecadorComunicacion/ViewLocator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.DependencyInjection;
+
+namespace ChecadorComunicacion;
+
+public class ViewLocator : IDataTemplate
+{
+    private readonly Dictionary<Type, Func<Control?>> _locator = new();
+
+    public Control Build(object? data)
+    {
+        var name = data.GetType().FullName!.Replace("ViewModel", "View");
+        var type = Type.GetType(name);
+
+        if (type != null)
+        {
+            return (Control)Activator.CreateInstance(type);
+        }
+
+        return new TextBlock { Text = $"Not Found: {name}" };
+    }
+
+    public bool Match(object? data)
+    {
+        return data is ObservableObject;
+    }
+
+    private void RegisterViewFactory<TViewModel, TView>()
+        where TViewModel : class
+        where TView : Control
+        => _locator.Add(
+            typeof(TViewModel),
+            Design.IsDesignMode
+                ? Activator.CreateInstance<TView>
+                : Ioc.Default.GetService<TView>);
+}
