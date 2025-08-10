efb38827284df298ca2c4c00e8aedf768d110dd9
Ernesto Amaral <erneto.amaral13@gmail.com>
2025-08-09 18:42:27 -0700

    Add MainWindow XAML and code-behind for the main application interface

---
+++ ChecadorComunicacion/Views/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using ChecadorComunicacion.ViewModels;
+using FluentAvalonia.UI.Windowing;
+
+namespace ChecadorComunicacion;
+
+public partial class MainWindow : AppWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+    }
+}
