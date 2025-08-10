d9ad2c4e2609415a383c53fe3a7d5b1f98e5523f
Ernesto Amaral <erneto.amaral13@gmail.com>
2025-08-09 18:42:33 -0700

    Add MainWindowViewModel for managing application navigation and state

---
+++ ChecadorComunicacion/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ChecadorComunicacion.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    [ObservableProperty] private bool _isPaneOpen = true;
+
+    [ObservableProperty] private ViewModelBase _currentPage = new ChecadorViewModel();
+
+    [ObservableProperty] private ListItemTemplate? _selectedListItem;
+
+    partial void OnSelectedListItemChanged(ListItemTemplate? value)
+    {
+        if (value is null) return;
+        var instance = Activator.CreateInstance(value.ModelType);
+        if (instance is null) return;
+        CurrentPage = (ViewModelBase)instance;
+    }
+
+    public ObservableCollection<ListItemTemplate> Items { get; } = new()
+    {
+        new ListItemTemplate(typeof(ChecadorViewModel), "FingerprintRegular"),
+        new ListItemTemplate(typeof(PersonaViewModel), "PeopleTeamAddRegular"),
+        new ListItemTemplate(typeof(ReportesViewModel), "ReportDocumentRegular"),
+    };
+
+    [RelayCommand]
+    public void TriggerPane()
+    {
+        IsPaneOpen = !IsPaneOpen;
+    }
+}
+
+public class ListItemTemplate
+{
+    public ListItemTemplate(Type type, string iconKey)
+    {
+        ModelType = type;
+        Label = type.Name.Replace("ViewModel", "");
+
+        Application.Current!.TryFindResource(iconKey, out var res);
+        ListItemIcon = (StreamGeometry)res!;
+    }
+
+    public string Label { get; set; }
+    public Type ModelType { get; set; }
+    public StreamGeometry ListItemIcon { get; }
+}
