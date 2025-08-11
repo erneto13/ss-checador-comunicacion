08d28f748fb0309d5f808f89b9d001475eaa9009
Ernesto Amaral <erneto.amaral13@gmail.com>
2025-08-11 10:18:11 -0700

    Refactor MainWindow layout to use SplitView for improved navigation and add minimize/close functionality

--- ChecadorComunicacion/Views/MainWindow.axaml.cs
+++ ChecadorComunicacion/Views/MainWindow.axaml.cs
@@ -1,13 +1,49 @@
+using System.Linq;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
 using ChecadorComunicacion.ViewModels;
-using FluentAvalonia.UI.Windowing;
+using FluentAvalonia.UI.Controls;
 
 namespace ChecadorComunicacion;
 
-public partial class MainWindow : AppWindow
+public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
 
+        this.Loaded += MainWindow_Loaded;
+    }
+
+    private void MainWindow_Loaded(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        if (this.FindControl<NavigationView>("NavigationView") is NavigationView navView)
+        {
+            navView.SelectionChanged += NavigationView_SelectionChanged;
+        }
+    }
+
+    private void NavigationView_SelectionChanged(object? sender, NavigationViewSelectionChangedEventArgs e)
+    {
+        if (e.SelectedItem is NavigationViewItem item &&
+            item.Tag is string tagName &&
+            DataContext is MainWindowViewModel viewModel)
+        {
+            var listItem = viewModel.Items.FirstOrDefault(x => x.ModelType.Name == tagName);
+            if (listItem != null)
+            {
+                viewModel.SelectedListItem = listItem;
+            }
+        }
+    }
+
+    private void CloseApp_Click(object? sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void MinimizeApp_Click(object? sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
     }
 }
