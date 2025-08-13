16e13dbb7386e7fbbee3e579b089bc09d0dd4080
Ernesto Amaral <erneto.amaral13@gmail.com>
2025-08-12 21:17:04 -0700

    Add ConfirmDialogView and ViewModel for confirmation dialog functionality

---
+++ ChecadorComunicacion/ViewModels/ConfirmDialogViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ChecadorComunicacion.ViewModels;
+
+public partial class ConfirmDialogViewModel : DialogViewModel
+{
+    [ObservableProperty] private string _title = "Confirmación";
+    [ObservableProperty] private string _message = "¿Estás seguro de esta acción?";
+    [ObservableProperty] private string _confirmText = "Si";
+    [ObservableProperty] private string _cancelText = "No";
+    [ObservableProperty] private string _iconText = "\xe7ba";
+
+    [ObservableProperty] private bool _confirmed;
+
+    [RelayCommand]
+    public void Confirm()
+    {
+        Confirmed = true;
+        Close();
+    }
+
+    [RelayCommand]
+    public void Cancel()
+    {
+        Confirmed = false;
+        Close();
+    }
+}
---
+++ ChecadorComunicacion/ViewModels/DialogViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ChecadorComunicacion.ViewModels;
+
+public partial class DialogViewModel : ViewModelBase
+{
+    [ObservableProperty] private bool _isDialogOpen;
+
+    protected TaskCompletionSource CloseTask = new();
+
+    public async Task WaitAsync()
+    {
+        await CloseTask.Task;
+    }
+
+    public void Open()
+    {
+        if (CloseTask.Task.IsCompleted)
+            CloseTask = new TaskCompletionSource();
+
+        IsDialogOpen = true;
+    }
+
+    protected void Close()
+    {
+        IsDialogOpen = false;
+        CloseTask.TrySetResult();
+    }
+}
