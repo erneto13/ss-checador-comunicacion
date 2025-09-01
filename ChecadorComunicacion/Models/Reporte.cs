7a5b03c3bbc0037545c3ef6b42fc33c847552fb0
Ernesto Amaral <erneto.amaral13@gmail.com>
2025-09-01 13:36:21 -0700

    Add Reporte class for managing attendance report data

---
+++ ChecadorComunicacion/Models/Reporte.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ChecadorComunicacion.Models;
+
+public class Reporte
+{
+    public DateOnly Fecha { get; set; }
+    public string Matricula { get; set; }
+    public string Nombre { get; set; }
+    public string TipoPersona { get; set; }
+    public string HoraEntrada { get; set; }
+    public string HoraSalida { get; set; }
+    public double HorasTrabajadas { get; set; }
+}
