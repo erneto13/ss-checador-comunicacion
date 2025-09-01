7b28e5dd76461ee34d0d7fa6f6caa324e579d9b2
Ernesto Amaral <erneto.amaral13@gmail.com>
2025-09-01 13:35:54 -0700

    Add MessageService for displaying error messages in a dialog

---
+++ ChecadorComunicacion/Helpers/MessageService.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+
+namespace ChecadorComunicacion.Helpers;
+
+public static class MessageService
+{
+    public static async Task ShowErrorAsync(string message)
+    {
+        
+        var messageBox = new Window
+        {
+            Title = "Error",
+            Content = new TextBlock { Text = message },
+            Height = 500,
+            Width = 300,
+            WindowStartupLocation = WindowStartupLocation.CenterScreen
+        };
+        await messageBox.ShowDialog((Window)Avalonia.Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop ? desktop.MainWindow : null);
+    }
+    
+    
+}
